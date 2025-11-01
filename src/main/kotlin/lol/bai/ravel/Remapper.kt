1a328509c5cd17b8d5852560b61237de0082a480
Dimas Firmansyah <deirn@bai.lol>
2025-11-02 02:19:24 +0700

    don't use ReferenceSearcher

--- src/main/kotlin/lol/bai/ravel/Remapper.kt
+++ src/main/kotlin/lol/bai/ravel/Remapper.kt
@@ -1,16 +1,12 @@
 package lol.bai.ravel
 
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.rootManager
-import com.intellij.openapi.util.Computable
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.isFile
 import com.intellij.psi.*
-import com.intellij.psi.codeStyle.JavaCodeStyleManager
-import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.psi.util.PsiTreeUtil
 import net.fabricmc.mappingio.tree.MappingTree.*
 
@@ -67,25 +63,9 @@ private fun Mappings.newName(method: MethodMapping): String? {
     return if (methodName == method.srcName) null else methodName
 }
 
-private fun forEachRefs(runners: MutableList<Runnable>, module: Module, elt: PsiElement, action: (PsiElement) -> Unit) {
-    val app = ApplicationManager.getApplication()
-    val refs = app.runReadAction(Computable {
-        ReferencesSearch.search(elt, module.moduleScope).findAll()
-    })
-
-    refs.forEach {
-        runners.add {
-            try {
-                action(it.element)
-            } catch (_: Exception) {
-            }
-        }
-    }
-}
-
 /**
- * TODO: Currently tested with Fabric api module
- *  - method overrides
+ * TODO: Currently tested with WTHIT
+ *  - nested class replaced with fully qualified name
  *  - mixin
  *  - access widener
  *  - kotlin
@@ -94,137 +74,142 @@ fun remap(project: Project, model: RemapperModel) {
     val psi = PsiManager.getInstance(project)
     val java = JavaPsiFacade.getInstance(project)
     val javaFactory = java.elementFactory
-    val javaStyle = JavaCodeStyleManager.getInstance(project)
 
-    val mAllClasses = linkedMapOf<String, ClassMapping>()
+    val mClasses = linkedMapOf<String, ClassMapping>()
     model.mappings.first().tree.classes.forEach {
-        mAllClasses[replaceQualifier(it.srcName)] = it
+        mClasses[replaceQualifier(it.srcName)] = it
     }
 
-    for (module in model.modules) {
-        val writers = arrayListOf<Runnable>()
-        val mTargetClasses = hashSetOf<String>()
-        val mTargetFields = hashMapOf<String, MutableSet<String>>()
-        val mTargetMethods = hashMapOf<String, MutableSet<String>>()
+    fun newMethodName(pMethod: PsiMethod): String? {
+        val pMethod = pMethod.findDeepestSuperMethods().firstOrNull() ?: pMethod
+        val pClass = pMethod.containingClass ?: return null
+        val pClassName = pClass.qualifiedName ?: return null
+        val mClass = mClasses[pClassName] ?: return null
 
-        module.rootManager.sourceRoots.forEach { root ->
-            VfsUtil.iterateChildrenRecursively(root, null) vf@{ vf ->
-                if (!vf.isFile) return@vf true
-                if (vf.extension != "java") return@vf true
-
-                val javaFile = psi.findFile(vf) ?: return@vf true
-                if (javaFile !is PsiJavaFile) return@vf true
-
-                PsiTreeUtil.processElements(javaFile, PsiJavaCodeReferenceElement::class.java) ref@{ ref ->
-                    val target = ref.resolve() ?: return@ref true
-                    val pClass = when (target) {
-                        is PsiClass -> target
-                        is PsiMember -> target.containingClass ?: return@ref true
-                        else -> return@ref true
-                    }
-
-                    val className = pClass.qualifiedName ?: return@ref true
-                    if (!mAllClasses.containsKey(className)) return@ref true
-                    mTargetClasses.add(className)
-
-                    val memberName = target.name ?: return@ref true
-                    val members = when (target) {
-                        is PsiField -> mTargetFields
-                        is PsiMethod -> mTargetMethods
-                        else -> return@ref true
-                    }
-                    members.computeIfAbsent(className) { hashSetOf() }
-                        .add(memberName)
-
-                    return@ref true
-                }
-                return@vf true
-            }
+        val mSignatureBuilder = StringBuilder()
+        mSignatureBuilder.append("(")
+        for (pParam in pMethod.parameterList.parameters) {
+            mSignatureBuilder.append(pParam.type.toRaw())
         }
+        mSignatureBuilder.append(")")
+        val pReturn = pMethod.returnType ?: PsiTypes.voidType()
+        mSignatureBuilder.append(pReturn.toRaw())
+        val mSignature = mSignatureBuilder.toString()
 
-        for ((oldClassName, mClass) in mAllClasses) {
-            if (!mTargetClasses.contains(oldClassName)) continue
+        val mMethod = mClass.getMethod(pMethod.name, mSignature) ?: return null
+        val newMethodName = model.mappings.newName(mMethod) ?: return null
 
-            val scope = module.getModuleWithDependenciesAndLibrariesScope(true)
-            val pClass = java.findClass(oldClassName, scope) ?: continue
-            val newClassName = model.mappings.newName(mClass)
+        return newMethodName
+    }
 
-            for (mField in mClass.fields) {
-                if (mTargetFields[oldClassName]?.contains(mField.srcName) != true) continue
+    val writers = arrayListOf<Runnable>()
 
-                val pField = pClass.findFieldByName(mField.srcName, false) ?: continue
-                val newFieldName = model.mappings.newName(mField) ?: continue
+    for (module in model.modules) for (root in module.rootManager.sourceRoots) VfsUtil.iterateChildrenRecursively(root, null) vf@{ vf ->
+        if (!vf.isFile) return@vf true
+        if (vf.extension != "java") return@vf true
 
-                forEachRefs(writers, module, pField) ref@{ elt ->
-                    val callRef = PsiTreeUtil.getParentOfType(elt, PsiJavaCodeReferenceElement::class.java, false)
-                    if (callRef != null) {
-                        val refElt = callRef.referenceNameElement!!
-                        refElt.replace(javaFactory.createExpressionFromText(newFieldName, refElt))
-                        return@ref
-                    }
+        val javaFile = psi.findFile(vf) ?: return@vf true
+        if (javaFile !is PsiJavaFile) return@vf true
+
+        PsiTreeUtil.processElements(javaFile, PsiJavaCodeReferenceElement::class.java) r@{ pRef ->
+            val pTarget = pRef.resolve() ?: return@r true
+
+            if (pTarget is PsiField) {
+                val pClass = pTarget.containingClass ?: return@r true
+                val pClassName = pClass.qualifiedName ?: return@r true
+                val mClass = mClasses[pClassName] ?: return@r true
+                val mField = mClass.getField(pTarget.name, null) ?: return@r true
+                val newFieldName = model.mappings.newName(mField) ?: return@r true
+
+                val pRefElt = pRef.referenceNameElement!!
+                writers.add {
+                    pRefElt.replace(javaFactory.createExpressionFromText(newFieldName, pRefElt))
                 }
+                return@r true
             }
 
-            for (mMethod in mClass.methods) {
-                if (mTargetMethods[oldClassName]?.contains(mMethod.srcName) != true) continue
-
-                val newMethodName = model.mappings.newName(mMethod) ?: continue
-                val (mParams, mReturn) = parseJvmDescriptor(mMethod.srcDesc!!)
-
-                method@ for (pMethod in pClass.findMethodsByName(mMethod.srcName, false)) {
-                    val pReturn = pMethod.returnType ?: PsiTypes.voidType()
-                    if (pReturn.toRaw() != mReturn) continue
-
-                    val pParams = pMethod.parameterList.parameters
-                    if (pParams.size != mParams.size) continue
-                    for (i in pParams.indices) {
-                        val pParam = pParams[i]
-                        val mParam = mParams[i]
-                        if (pParam.type.toRaw() != mParam) continue@method
-                    }
-
-                    forEachRefs(writers, module, pMethod) ref@{ elt ->
-                        val callRef = PsiTreeUtil.getParentOfType(elt, PsiJavaCodeReferenceElement::class.java, false)
-                        if (callRef != null) {
-                            val refElt = callRef.referenceNameElement!!
-                            refElt.replace(javaFactory.createExpressionFromText(newMethodName, refElt))
-                            return@ref
-                        }
-                    }
+            if (pTarget is PsiMethod) {
+                val newMethodName = newMethodName(pTarget) ?: return@r true
+                val pRefElt = pRef.referenceNameElement!!
+                writers.add {
+                    pRefElt.replace(javaFactory.createExpressionFromText(newMethodName, pRefElt))
                 }
+                return@r true
             }
 
-            if (newClassName == null) continue
-            forEachRefs(writers, module, pClass) ref@{ elt ->
-                val callRef = PsiTreeUtil.getParentOfType(elt, PsiJavaCodeReferenceElement::class.java, false)
-                if (callRef != null) {
-                    val newRefName = newClassName.substringAfterLast('.')
-                    val refElt = callRef.referenceNameElement!!
+            if (pTarget is PsiClass) {
+                val pClassName = pTarget.qualifiedName ?: return@r true
+                val mClass = mClasses[pClassName] ?: return@r true
+                val newClassName = model.mappings.newName(mClass) ?: return@r true
+                val refElt = pRef.referenceNameElement!!
 
+                val newRefName = newClassName.substringAfterLast('.')
+                writers.add {
                     // TODO: Malformed type errors, seem to be only a log messages
+                    //       Can't seem to be caught using try-catch block
                     refElt.replace(javaFactory.createExpressionFromText(newRefName, refElt))
+                }
 
-                    val refQual = callRef.qualifier
-                    if (refQual != null) {
-                        val newQualName = newClassName.substringBeforeLast('.')
+                val refQual = pRef.qualifier
+                if (refQual != null) {
+                    val newQualName = newClassName.substringBeforeLast('.')
+                    writers.add {
                         refQual.replace(javaFactory.createExpressionFromText(newQualName, refQual))
                     }
-
-                    return@ref
                 }
+                return@r true
             }
+
+            return@r true
         }
 
+        PsiTreeUtil.processElements(javaFile, PsiMethod::class.java) m@{ pMethod ->
+            val newMethodName = newMethodName(pMethod) ?: return@m true
+            writers.add {
+                pMethod.name = newMethodName
+            }
+            return@m true
+        }
+
+        return@vf true
+    }
+
+    writers.forEach { writer ->
         WriteCommandAction.runWriteCommandAction(project, "Ravel Remapper", null, {
-            writers.forEach { it.run() }
+            @Suppress("UnusedExpression")
+            try {
+                writer.run()
+            } catch (e: Exception) {
+                // TODO: where is the culprit of errors?
+                e
+            }
         })
     }
 }
 
+@Suppress("UnstableApiUsage")
 private fun PsiType.toRaw(): String {
     return when (this) {
-        is PsiArrayType -> componentType.toRaw() + "[]"
-        is PsiPrimitiveType -> name
+        is PsiArrayType -> "[" + componentType.toRaw()
+        is PsiPrimitiveType -> kind.binaryName
+        is PsiClassType -> {
+            fun rawName(cls: PsiClass): String? {
+                if (cls is PsiTypeParameter) {
+                    val bounds = cls.extendsList.referencedTypes
+                    if (bounds.isEmpty()) return "Ljava/lang/Object;"
+                    return rawName(bounds.first().resolve()!!)
+                }
+
+                val fullName = cls.qualifiedName ?: return null
+                val parent = cls.containingClass
+                if (parent != null) return rawName(parent) + "$" + cls.name
+                return fullName.replace('.', '/')
+            }
+
+            val name = rawName(resolve()!!)
+            "L${name};"
+        }
+
         else -> {
             val ret = canonicalText
             if (ret.contains('<')) ret.substringBefore('<') else ret
@@ -232,41 +217,3 @@ private fun PsiType.toRaw(): String {
     }
 }
 
-private fun parseJvmDescriptor(descriptor: String): Pair<List<String>, String> {
-    var i = 0
-    fun parseType(): String {
-        return when (val c = descriptor[i++]) {
-            'B' -> "byte"
-            'C' -> "char"
-            'D' -> "double"
-            'F' -> "float"
-            'I' -> "int"
-            'J' -> "long"
-            'S' -> "short"
-            'Z' -> "boolean"
-            'V' -> "void"
-            '[' -> {
-                val comp = parseType()
-                "$comp[]"
-            }
-
-            'L' -> {
-                val start = i
-                val semicolon = descriptor.indexOf(';', start)
-                val internal = descriptor.substring(start, semicolon)
-                i = semicolon + 1
-                internal.replace('/', '.')
-            }
-
-            else -> throw IllegalArgumentException("Unknown descriptor char: $c in $descriptor")
-        }
-    }
-
-    if (descriptor.isEmpty() || descriptor[0] != '(') throw IllegalArgumentException("Bad descriptor: $descriptor")
-    i = 1
-    val params = mutableListOf<String>()
-    while (descriptor[i] != ')') params += parseType()
-    i++ // skip ')'
-    val ret = parseType()
-    return params to ret
-}
